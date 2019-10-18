2b518f93643a8bb443104dc4d9e7acbef8a98b3b
Niklas Salmoukas <niklas@salmoukas.com>
2019-10-18 14:49:32 +0200

    more autoreleasepool?

--- src/macos/webkit.m
+++ src/macos/webkit.m
@@ -7,57 +7,64 @@
 #include "../inner.h"
 
 bool audience_inner_init() {
-  [NSApplication sharedApplication];
-  return true;
+  @autoreleasepool {
+    [NSApplication sharedApplication];
+    return true;
+  }
 }
 
 void *audience_inner_window_create(const wchar_t *const title,
                                    const wchar_t *const url) {
+  @autoreleasepool {
+    NSWindow *window =
+        [[NSWindow alloc] initWithContentRect:CGRectMake(0, 0, 500, 500)
+                                    styleMask:NSWindowStyleMaskTitled |
+                                              NSWindowStyleMaskClosable |
+                                              NSWindowStyleMaskMiniaturizable |
+                                              NSWindowStyleMaskResizable
+                                      backing:NSBackingStoreBuffered
+                                        defer:NO];
+    [window setTitle:[[NSString alloc]
+                         initWithBytes:title
+                                length:wcslen(title) * sizeof(*title)
+                              encoding:NSUTF32LittleEndianStringEncoding]];
+    [window center];
 
-  NSWindow *window = [[NSWindow alloc]
-      initWithContentRect:CGRectMake(0, 0, 500, 500)
-                styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable |
-                          NSWindowStyleMaskMiniaturizable |
-                          NSWindowStyleMaskResizable
-                  backing:NSBackingStoreBuffered
-                    defer:NO];
-  [window setTitle:[[NSString alloc]
-                       initWithBytes:title
-                              length:wcslen(title) * sizeof(*title)
-                            encoding:NSUTF32LittleEndianStringEncoding]];
-  [window center];
+    WKWebViewConfiguration *config = [WKWebViewConfiguration new];
 
-  WKWebViewConfiguration *config = [WKWebViewConfiguration new];
+    WKWebView *webview =
+        [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, 500, 500)
+                           configuration:config];
+    [webview
+        loadRequest:
+            [NSURLRequest
+                requestWithURL:
+                    [NSURL
+                        URLWithString:
+                            [[NSString alloc]
+                                initWithBytes:url
+                                       length:wcslen(url) * sizeof(*url)
+                                     encoding:
+                                         NSUTF32LittleEndianStringEncoding]]]];
+    [webview setAutoresizesSubviews:YES];
+    [webview setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
 
-  WKWebView *webview =
-      [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, 500, 500)
-                         configuration:config];
-  [webview
-      loadRequest:
-          [NSURLRequest
-              requestWithURL:
-                  [NSURL URLWithString:
-                             [[NSString alloc]
-                                 initWithBytes:url
-                                        length:wcslen(url) * sizeof(*url)
-                                      encoding:
-                                          NSUTF32LittleEndianStringEncoding]]]];
-  [webview setAutoresizesSubviews:YES];
-  [webview setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    [[window contentView] addSubview:webview];
+    [window orderFrontRegardless];
 
-  [[window contentView] addSubview:webview];
-  [window orderFrontRegardless];
+    [[NSApplication sharedApplication]
+        setActivationPolicy:NSApplicationActivationPolicyRegular];
+    [[NSApplication sharedApplication] finishLaunching];
+    [[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
 
-  [[NSApplication sharedApplication]
-      setActivationPolicy:NSApplicationActivationPolicyRegular];
-  [[NSApplication sharedApplication] finishLaunching];
-  [[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
-
-  return (__bridge_retained void *)window;
+    return (__bridge_retained void *)window;
+  }
 }
 
 void audience_inner_window_destroy(void *window) {
-  NSWindow *w = (__bridge_transfer NSWindow *)window;
+  @autoreleasepool {
+    NSWindow *w = (__bridge_transfer NSWindow *)window;
+  }
 }
 
 void audience_inner_loop() {
