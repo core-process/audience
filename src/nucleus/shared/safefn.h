e0d3570a6512606253e4332ae409fe5f94a2cc84
Niklas Salmoukas <niklas@salmoukas.com>
2019-10-21 23:05:07 +0200

    fix msvc macro expansion

--- src/nucleus/shared/safefn.h
+++ src/nucleus/shared/safefn.h
@@ -3,8 +3,10 @@
 #include "../../common/safefn.h"
 #include "nucleus.h"
 
+#define NUCLEUS_SAFE_FN_EXPAND( x ) x
+
 #if defined(NUCLEUS_TRANSLATE_EXCEPTIONS)
-#define NUCLEUS_SAFE_FN(...) SAFE_FN(__VA_ARGS__, NUCLEUS_TRANSLATE_EXCEPTIONS)
+#define NUCLEUS_SAFE_FN(...) NUCLEUS_SAFE_FN_EXPAND(SAFE_FN(__VA_ARGS__, NUCLEUS_TRANSLATE_EXCEPTIONS))
 #else
-#define NUCLEUS_SAFE_FN(...) SAFE_FN(__VA_ARGS__)
+#define NUCLEUS_SAFE_FN(...) NUCLEUS_SAFE_FN_EXPAND(SAFE_FN(__VA_ARGS__))
 #endif
