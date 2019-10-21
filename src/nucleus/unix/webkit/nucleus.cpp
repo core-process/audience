2df41001bd957bc0744ecfea26f601161814bfd9
Niklas Salmoukas <niklas@salmoukas.com>
2019-10-22 01:26:52 +0200

    improve create window procedure

--- src/nucleus/unix/webkit/nucleus.cpp
+++ src/nucleus/unix/webkit/nucleus.cpp
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "../../../common/trace.h"
+#include "../../../common/scope_guard.h"
 #include "../../shared/interface.h"
 #include "nucleus.h"
 
@@ -27,6 +28,9 @@ bool internal_init()
 
 AudienceHandle *internal_window_create(const std::wstring &title, const std::wstring &url)
 {
+  scope_guard scope_fail(scope_guard::execution::exception);
+
+  // create handle instance
   auto handle = std::make_shared<AudienceHandleData>();
 
   // convert input parameter
@@ -42,6 +46,20 @@ AudienceHandle *internal_window_create(const std::wstring &title, const std::wst
 
   // create window
   handle->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+  if (handle->window == nullptr)
+  {
+    throw std::runtime_error("could not create window");
+  }
+
+  scope_fail += [handle]() {
+    if (handle->window != nullptr)
+    {
+      gtk_widget_destroy(GTK_WIDGET(handle->window));
+      handle->window = nullptr;
+    }
+  };
+
   gtk_window_set_title(GTK_WINDOW(handle->window), titlea.c_str());
   gtk_window_set_default_size(GTK_WINDOW(handle->window), workarea.width / 2, workarea.height / 2);
   gtk_window_set_resizable(GTK_WINDOW(handle->window), true);
@@ -49,6 +67,12 @@ AudienceHandle *internal_window_create(const std::wstring &title, const std::wst
 
   // create webview
   handle->webview = webkit_web_view_new();
+
+  if (handle->webview == nullptr)
+  {
+    throw std::runtime_error("could not create webview");
+  }
+
   gtk_container_add(GTK_CONTAINER(handle->window), GTK_WIDGET(handle->webview));
 
   // create handle instance for window and widget
@@ -57,9 +81,9 @@ AudienceHandle *internal_window_create(const std::wstring &title, const std::wst
   g_object_set_data(G_OBJECT(handle->webview), WIDGET_HANDLE_KEY, handle_priv);
 
   // listen to destroy signal and title changed event
-  g_signal_connect(G_OBJECT(handle->window), "destroy", G_CALLBACK(widget_destroy_callback), nullptr);
-  g_signal_connect(G_OBJECT(handle->webview), "destroy", G_CALLBACK(widget_destroy_callback), nullptr);
-  g_signal_connect(G_OBJECT(handle->webview), "notify::title", G_CALLBACK(webview_title_update_callback), nullptr);
+  g_signal_connect(G_OBJECT(handle->window), "destroy", G_CALLBACK(NUCLEUS_SAFE_FN(widget_destroy_callback)), nullptr);
+  g_signal_connect(G_OBJECT(handle->webview), "destroy", G_CALLBACK(NUCLEUS_SAFE_FN(widget_destroy_callback)), nullptr);
+  g_signal_connect(G_OBJECT(handle->webview), "notify::title", G_CALLBACK(NUCLEUS_SAFE_FN(webview_title_update_callback)), nullptr);
 
   // show window and trigger url load
   webkit_web_view_load_uri(WEBKIT_WEB_VIEW(handle->webview), urla.c_str());
