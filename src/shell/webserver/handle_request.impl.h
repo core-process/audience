d145040b5a54ab6c528e411a2787fee00aa23fdb
Niklas Salmoukas <niklas@salmoukas.com>
2019-10-25 04:46:29 +0200

    webapp messaging implementation

--- src/shell/webserver/handle_request.impl.h
+++ src/shell/webserver/handle_request.impl.h
@@ -6,6 +6,9 @@
 #include "mime_type.impl.h"
 #include "path_util.impl.h"
 
+extern const char *_audience_webapp_messaging_websocket_code_begin;
+extern std::size_t _audience_webapp_messaging_websocket_code_length;
+
 // This function produces an HTTP response for the given
 // request. The type of the response object depends on the
 // contents of the request, so the interface requires the
@@ -72,45 +75,77 @@ void handle_request(
   {
     target = target.substr(0, qmi);
   }
-  std::string path = path_cat(doc_root, target);
-  if (target.back() == '/')
-    path.append("index.html");
 
-  // Attempt to open the file
-  boost::beast::error_code ec;
-  boost::beast::http::file_body::value_type body;
-  body.open(path.c_str(), boost::beast::file_mode::scan, ec);
-
-  // Handle the case where the file doesn't exist
-  if (ec == boost::beast::errc::no_such_file_or_directory)
-    return send(not_found(target));
-
-  // Handle an unknown error
-  if (ec)
-    return send(server_error(ec.message()));
-
-  // Cache the size since we need it after the move
-  auto const size = body.size();
-
-  // Respond to HEAD request
-  if (req.method() == boost::beast::http::verb::head)
+  // handle case: /audience.js
+  if (target == "/audience.js")
   {
-    boost::beast::http::response<boost::beast::http::empty_body> res{boost::beast::http::status::ok, req.version()};
+    // Cache the size since we need it after the move
+    auto const size = _audience_webapp_messaging_websocket_code_length;
+
+    // Respond to HEAD request
+    if (req.method() == boost::beast::http::verb::head)
+    {
+      boost::beast::http::response<boost::beast::http::empty_body> res{boost::beast::http::status::ok, req.version()};
+      res.set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
+      res.set(boost::beast::http::field::content_type, mime_type(target));
+      res.content_length(size);
+      res.keep_alive(req.keep_alive());
+      return send(std::move(res));
+    }
+
+    // Respond to GET request
+    boost::beast::http::response<boost::beast::http::string_body> res{boost::beast::http::status::ok, req.version()};
+    res.set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(boost::beast::http::field::content_type, mime_type(target));
+    res.keep_alive(req.keep_alive());
+    res.body() = std::string(_audience_webapp_messaging_websocket_code_begin, size);
+    res.prepare_payload();
+    return send(std::move(res));
+  }
+  // handle filesystem case
+  else
+  {
+    // build path
+    std::string path = path_cat(doc_root, target);
+    if (target.back() == '/')
+      path.append("index.html");
+
+    // Attempt to open the file
+    boost::beast::error_code ec;
+    boost::beast::http::file_body::value_type body;
+    body.open(path.c_str(), boost::beast::file_mode::scan, ec);
+
+    // Handle the case where the file doesn't exist
+    if (ec == boost::beast::errc::no_such_file_or_directory)
+      return send(not_found(target));
+
+    // Handle an unknown error
+    if (ec)
+      return send(server_error(ec.message()));
+
+    // Cache the size since we need it after the move
+    auto const size = body.size();
+
+    // Respond to HEAD request
+    if (req.method() == boost::beast::http::verb::head)
+    {
+      boost::beast::http::response<boost::beast::http::empty_body> res{boost::beast::http::status::ok, req.version()};
+      res.set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
+      res.set(boost::beast::http::field::content_type, mime_type(path));
+      res.content_length(size);
+      res.keep_alive(req.keep_alive());
+      return send(std::move(res));
+    }
+
+    // Respond to GET request
+    boost::beast::http::response<boost::beast::http::file_body> res{
+        std::piecewise_construct,
+        std::make_tuple(std::move(body)),
+        std::make_tuple(boost::beast::http::status::ok, req.version())};
     res.set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
     res.set(boost::beast::http::field::content_type, mime_type(path));
     res.content_length(size);
     res.keep_alive(req.keep_alive());
     return send(std::move(res));
   }
-
-  // Respond to GET request
-  boost::beast::http::response<boost::beast::http::file_body> res{
-      std::piecewise_construct,
-      std::make_tuple(std::move(body)),
-      std::make_tuple(boost::beast::http::status::ok, req.version())};
-  res.set(boost::beast::http::field::server, BOOST_BEAST_VERSION_STRING);
-  res.set(boost::beast::http::field::content_type, mime_type(path));
-  res.content_length(size);
-  res.keep_alive(req.keep_alive());
-  return send(std::move(res));
 }
