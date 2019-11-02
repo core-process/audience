4852023c7b95ed010272627564062b7535ec49e5
Niklas Salmoukas <niklas@salmoukas.com>
2019-11-02 15:58:05 +0100

    improve logging config

--- src/common/logger.h
+++ src/common/logger.h
@@ -1,8 +1,16 @@
 #pragma once
 
 #include <mutex>
+
 #include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+#include <spdlog/sinks/dist_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#ifdef WIN32
+#include <spdlog/sinks/msvc_sink.h>
+#else
+#include <spdlog/sinks/syslog_sink.h>
+#endif
 
 inline void setup_logger(std::string name)
 {
@@ -11,10 +19,26 @@ inline void setup_logger(std::string name)
   std::lock_guard<std::mutex> lock(mutex);
   if (!initialized)
   {
-    auto logger = spdlog::stdout_color_mt(name);
+    // prepare sinks
+    auto dist_sink = std::make_shared<spdlog::sinks::dist_sink_mt>();
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>(spdlog::color_mode::automatic);
+#ifdef AUDIENCE_LOG_LEVEL_CONSOLE
+    console_sink->set_level(spdlog::level::level_enum(AUDIENCE_LOG_LEVEL_CONSOLE));
+#endif
+    dist_sink->add_sink(console_sink);
+#ifdef WIN32
+    dist_sink->add_sink(std::make_shared<spdlog::sinks::msvc_sink_mt>());
+#else
+    dist_sink->add_sink(std::make_shared<spdlog::sinks::syslog_sink_mt>(name, 0, LOG_USER, false));
+#endif
+    // prepare async logger
+    spdlog::init_thread_pool(8192, 1);
+    auto logger = std::make_shared<spdlog::async_logger>(name, dist_sink, spdlog::thread_pool(), spdlog::async_overflow_policy::block);
     logger->flush_on(spdlog::level::err);
+    // set default logger and log level
     spdlog::set_default_logger(logger);
     spdlog::set_level(spdlog::level::trace);
+    // done
     SPDLOG_DEBUG("audience logger '{}' initialized", name);
     initialized = true;
   }
