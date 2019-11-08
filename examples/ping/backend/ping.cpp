73d7fae20479dcb1723300f7839664a01c6d505a
Niklas Salmoukas <niklas@salmoukas.com>
2019-11-08 10:00:14 -0800

    streamline ping implementation

--- examples/ping/backend/ping.cpp
+++ examples/ping/backend/ping.cpp
@@ -4,63 +4,35 @@
 #include <functional>
 #include <cstdint>
 #include <cstring>
+
 #ifdef _WIN32
 #include <winsock2.h>
-#include <ws2tcpip.h>
 #else
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #endif
 
 #include "ping.h"
 
-#if defined(__APPLE__) || defined(_WIN32)
-#define SOL_IP IPPROTO_IP
-#endif
-
-#if !defined(_WIN32)
-#define closesocket close
-#endif
-
-#ifdef _WIN32
-#define ICMP_ECHO 8
-#endif
-
 using namespace std::chrono_literals;
+
+#pragma pack(push)
+#pragma pack(1)
 
 struct echo_package
 {
-#ifdef _WIN32
-  typedef struct icmphdr
+  struct
   {
-    uint8_t type; /* message type */
-    uint8_t code; /* type sub-code */
+    uint8_t type;
+    uint8_t code;
     uint16_t checksum;
-    union {
-      struct
-      {
-        uint16_t id;
-        uint16_t sequence;
-      } echo;           /* echo datagram */
-      uint32_t gateway; /* gateway address */
-      struct
-      {
-        uint16_t __unused;
-        uint16_t mtu;
-      } frag; /* path mtu discovery */
-    } un;
-  } icmp_header;
-#elif __APPLE__
-  typedef struct icmp icmp_header;
-#elif __linux__
-  typedef struct icmphdr icmp_header;
-#else
-#error "Unsupported target"
-#endif
-  icmp_header hdr;
-  char msg[64 - sizeof(icmp_header)];
+    uint16_t id;
+    uint16_t sequence;
+  } hdr;
+  char msg[64 - 8];
 };
+
+#pragma pack(pop)
 
 static uint16_t ping_package_checksum(echo_package &pkg);
 
@@ -95,52 +67,65 @@ void ping_start(
 
     // set ttl
     int ttl = 64;
+#if defined(__linux__)
     if (setsockopt(sock, SOL_IP, IP_TTL, (const char *)&ttl, sizeof(ttl)) != 0)
+#else
+    if (setsockopt(sock, IPPROTO_IP, IP_TTL, (const char *)&ttl, sizeof(ttl)) != 0)
+#endif
     {
       on_error("could not set ttl");
+#ifdef _WIN32
       closesocket(sock);
+#else
+      close(sock);
+#endif
       return;
     }
 
     // set receive timeout
-    timeval rcv_timeout;
+    timeval rcv_timeout{};
     rcv_timeout.tv_sec = 5;
     rcv_timeout.tv_usec = 0;
     if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char *)&rcv_timeout, sizeof rcv_timeout) != 0)
     {
       on_error("could not set receive timeout");
+#ifdef _WIN32
       closesocket(sock);
+#else
+      close(sock);
+#endif
       return;
     }
 
     // our ping target address: 8.8.8.8
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = 0x08080808;
+    if (inet_pton(AF_INET, "8.8.8.8", &addr.sin_addr) != 1)
+    {
+      on_error("could not parse ip address");
+#ifdef _WIN32
+      closesocket(sock);
+#else
+      close(sock);
+#endif
+      return;
+    }
+
+    // prepare echo package
+    echo_package pkg_send{};
+    pkg_send.hdr.type = 8;
+    pkg_send.hdr.id = htons(getpid());
+    for (size_t i = 0; i < sizeof(pkg_send.msg) - 1; i++)
+    {
+      pkg_send.msg[i] = i + '0';
+    }
 
     // ping loop
     for (uint16_t seq = 0; !ping_stop_signal; ++seq)
     {
-      // prepare echo package
-      echo_package pkg_send{};
-#ifdef __APPLE__
-      pkg_send.hdr.icmp_type = ICMP_ECHO;
-      pkg_send.hdr.icmp_id = getpid();
-      pkg_send.hdr.icmp_seq = seq;
-#else
-      pkg_send.hdr.type = ICMP_ECHO;
-      pkg_send.hdr.un.echo.id = getpid();
-      pkg_send.hdr.un.echo.sequence = seq;
-#endif
-      for (size_t i = 0; i < sizeof(pkg_send.msg) - 1; i++)
-      {
-        pkg_send.msg[i] = i + '0';
-      }
-#ifdef __APPLE__
-      pkg_send.hdr.icmp_cksum = ping_package_checksum(pkg_send);
-#else
+      // finalize echo package
+      pkg_send.hdr.sequence = htons(seq);
       pkg_send.hdr.checksum = ping_package_checksum(pkg_send);
-#endif
 
       // send echo package
       ping_time_point tp1 = std::chrono::system_clock::now();
@@ -153,10 +138,10 @@ void ping_start(
       }
 
       // receive echo reply package
-      echo_package pkg_receive{};
-      sockaddr_in rcv_addr;
+      echo_package pkg_rcv{};
+      sockaddr_in rcv_addr{};
       socklen_t rcv_addr_len = sizeof(rcv_addr);
-      if (recvfrom(sock, (char *)&pkg_receive, sizeof(pkg_receive), 0, (struct sockaddr *)&rcv_addr, &rcv_addr_len) <= 0)
+      if (recvfrom(sock, (char *)&pkg_rcv, sizeof(pkg_rcv), 0, (struct sockaddr *)&rcv_addr, &rcv_addr_len) <= 0)
       {
         on_error("could not receive echo reply package");
         std::this_thread::sleep_for(3s);
@@ -164,11 +149,7 @@ void ping_start(
       }
 
       // evaluate reply package
-#ifdef __APPLE__
-      if (pkg_receive.hdr.icmp_type == 0 && pkg_receive.hdr.icmp_code == 0 && std::memcmp(pkg_receive.msg, pkg_send.msg, sizeof(pkg_receive.msg)) == 0)
-#else
-      if (pkg_receive.hdr.type == 0 && pkg_receive.hdr.code == 0 && std::memcmp(pkg_receive.msg, pkg_send.msg, sizeof(pkg_receive.msg)) == 0)
-#endif
+      if (pkg_rcv.hdr.type == 0 && pkg_rcv.hdr.code == 0 && std::memcmp(pkg_rcv.msg, pkg_send.msg, sizeof(pkg_rcv.msg)) == 0)
       {
         ping_time_point tp2 = std::chrono::system_clock::now();
         ping_duration dur = tp2 - tp1;
@@ -176,6 +157,10 @@ void ping_start(
         if (dur.count() >= 0)
         {
           on_echo_reply(tp1, dur);
+        }
+        else
+        {
+          on_error("clock jump detected, skipping package");
         }
         std::this_thread::sleep_for(1s);
       }
@@ -187,7 +172,11 @@ void ping_start(
     }
 
     // close socket properly
+#ifdef _WIN32
     closesocket(sock);
+#else
+    close(sock);
+#endif
   });
 }
 
